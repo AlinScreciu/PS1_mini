cb1f8db4266bf9bfbbfbb1abff62e652a2acafe7
Zapper2321 <69618766+Zapper2321@users.noreply.github.com>
2021-03-21 22:53:20 +0200

    Update

--- main.c
+++ main.c
@@ -6,7 +6,6 @@ struct pc {
     int bool;
  };
 struct pc db[50];
-
 void add_pc() {
     int i;
     int c;
@@ -37,7 +36,7 @@ void add_pc() {
 };
 void show_pcs(){
     for ( int i = 0 ; i < 50 ; i++)
-        if ( db[i].bool==1 )
+        if ( db[i].bool == 1 )
             printf("%s,%s,%s,%s,%s,%s,%s,%s,%.2f\n",
                    db[i].type,db[i].name,db[i].cpu,db[i].ram,
                    db[i].storage,db[i].gpu,db[i].mobo,db[i].cooler,db[i].price);
@@ -51,7 +50,41 @@ void remove_pc_by_name() {
         if( strcmp(db[i].name,to_remove) == 0) db[i].bool = 0;
     }
 };
+void remove_pc_by_position() {
+    int last;
+    for ( int i  =  0 ; i < 50 ; i ++ )
+        if ( db[i].bool == 1 ) last = i;
+    int which;
+    printf("Enter 1 to remove the last pc added to the storage, and 2 to remove the first pc added to the storage.\n");
+    scanf("%d",&which);
+    switch ( which ) {
+        case 1:
+            db[last].bool = 0;
+            break;
+        case 2:
+            db[0].bool = 0;
+            break;
+        default:
+            printf("Invalid value.\n");
+            remove_pc_by_position();
+    }
+}
+void remove_pc() {
+    int c;
+    printf("Enter 1 to remove a pc from the storage by position, or 2 to remove a pc by name.\n");
+    scanf("%d",&c);
+    switch( c ) {
+        case 1:
+            remove_pc_by_position();
+            break;
+        case 2:
+            remove_pc_by_name();
+        default:
+            printf("Invalid value!\n");
+            break;
+    }
 
+}
 void menu() {
     printf("Actions:\n"
            "1. Add a computer.\n"
@@ -62,6 +95,7 @@ void menu() {
 int main() {
     for (int i = 0 ; i < 50 ; i++) db[i].bool = 0;
     while (1) {
+        for (int i = 0 ; i < 50 ; i++) printf("%d",db[i].bool);
         fflush(stdin);
         int choice;
         menu();
@@ -71,7 +105,7 @@ int main() {
                 add_pc();
                 break;
             case 2:
-                remove_pc_by_name();
+                remove_pc();
                 break;
             case 3:
                 show_pcs();
