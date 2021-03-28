f16e1ba433f4991316471f0681eb7835d0b02e6f
Zapper2321 <69618766+Zapper2321@users.noreply.github.com>
2021-03-28 16:16:21 +0300

    changed to double linked list
    //TODO
    remove by name/type etc.
    modify
    read/write to file
    protect to invalid file!
    some methods like avg price etc
    add a monitor mouse and keyboard struct

--- main.c
+++ main.c
@@ -1,119 +1,163 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 struct pc {
-    char type[15],name[30],cpu[40],ram[20],storage[20],gpu[50],mobo[40],cooler[30];
+    char type[128],name[128],cpu[128],ram[128],storage[128],gpu[128],motherboard[128];
     float price;
-    int bool;
  };
-struct pc db[50];
-void add_pc() {
-    int i;
-    int c;
-    for ( i = 0 ; i < 50 ; i ++ ) if ( ! db[i].bool ) break;
-    if ( i == 50 ) printf("No more space in the storage, remove a pc if you want to add another one!\n");
-    printf("PC type: \n");
-    scanf(" %[^\n]s",&db[i].type);
-    printf("PC name: \n");
-    scanf(" %[^\n]s",&db[i].name);
-    printf("PC CPU: \n");
-    scanf(" %[^\n]s",&db[i].cpu);
-    printf("PC RAM size: \n");
-    scanf(" %[^\n]s",&db[i].ram);
-    printf("PC storage size: \n");
-    scanf(" %[^\n]s",&db[i].storage);
-    printf("PC GPU: \n");
-    scanf(" %[^\n]s",&db[i].gpu);
-    printf("PC motherboard: \n");
-    scanf(" %[^\n]s",&db[i].mobo);
-    printf("CPU cooler: \n");
-    scanf(" %[^\n]s",&db[i].cooler);
-    printf("PC price: \n");
-    scanf("%f",&db[i].price);
-    printf("Enter 1 to add one more, or 0 to stop.\n");
-    scanf("%d",&c);
-    db[i].bool = 1;
-    if ( c == 1) add_pc();
+struct node {
+    struct pc* val;
+    struct node *next;
+    struct node *prev;
 };
-void show_pcs(){
-    for ( int i = 0 ; i < 50 ; i++)
-        if ( db[i].bool == 1 )
-            printf("%s,%s,%s,%s,%s,%s,%s,%s,%.2f\n",
-                   db[i].type,db[i].name,db[i].cpu,db[i].ram,
-                   db[i].storage,db[i].gpu,db[i].mobo,db[i].cooler,db[i].price);
+struct pc *make_pc(char* n,char* t,char* c,char* r,char* s,char* g,char* m,float p){
+    struct pc *temp = malloc(sizeof(struct pc));
+    strcpy(temp->type,t);
+    strcpy(temp->name,n);
+    strcpy(temp->cpu,c);
+    strcpy(temp->ram,r);
+    strcpy(temp->storage,s);
+    strcpy(temp->gpu,g);
+    strcpy(temp->motherboard,m);
+    temp->price = p;
+    return temp;
+}
+struct list{
+    struct node *first;
+    struct node *last;
+    int size;
+};
+struct node *make_node(char* n,char* t,char* c,char* r,char* s,char* g,char* m,float p){
+    struct node *temp = malloc(sizeof(struct node));
+    temp->next = malloc(sizeof(struct node));
+    temp->next = NULL;
+    temp->prev = NULL;
+    temp->val = make_pc(n,t,c,r,s,g,m,p);
+    return temp;
+}
+struct list make_list(){
+    struct list l;
+    l.first = malloc(sizeof(struct node));
+    l.last = NULL;
+    l.size = 0;
+    return l;
+}
 
-};
-void remove_pc_by_name() {
-    char to_remove[30];
-    printf("Enter the name of the pc you want to remove:\n");
-    scanf("%s",&to_remove);
-    for (int i = 0 ; i < 50 ; i++) {
-        if( strcmp(db[i].name,to_remove) == 0) db[i].bool = 0;
+void append(struct list *l, struct node* temp){
+    if (l->size == 0) {
+        struct node *first;
+        first = temp;
+        l->first = first;
+        l->size = l->size + 1;
     }
-};
-void remove_pc_by_position() {
-    int last;
-    for ( int i  =  0 ; i < 50 ; i ++ )
-        if ( db[i].bool == 1 ) last = i;
-    int which;
-    printf("Enter 1 to remove the last pc added to the storage, and 2 to remove the first pc added to the storage.\n");
-    scanf("%d",&which);
-    switch ( which ) {
-        case 1:
-            db[last].bool = 0;
-            break;
-        case 2:
-            db[0].bool = 0;
-            break;
-        default:
-            printf("Invalid value.\n");
-            remove_pc_by_position();
-    }
-}
-void remove_pc() {
-    int c;
-    printf("Enter 1 to remove a pc from the storage by position, or 2 to remove a pc by name.\n");
-    scanf("%d",&c);
-    switch( c ) {
-        case 1:
-            remove_pc_by_position();
-            break;
-        case 2:
-            remove_pc_by_name();
-        default:
-            printf("Invalid value!\n");
-            break;
-    }
-}
-void menu() {
-    printf("Actions:\n"
-           "1. Add a computer.\n"
-           "2. Remove a computer.\n"
-           "3. Show the computers.\n"
-           "0. Exit.\n");
-}
-int main() {
-    for (int i = 0 ; i < 50 ; i++) db[i].bool = 0;
-    while (1) {
-        for (int i = 0 ; i < 50 ; i++) printf("%d",db[i].bool);
-        fflush(stdin);
-        int choice;
-        menu();
-        scanf("%d",&choice);
-        switch (choice) {
-            case 1:
-                add_pc();
-                break;
-            case 2:
-                remove_pc();
-                break;
-            case 3:
-                show_pcs();
-                break;
-            case 0:
-                return 0;
-            default:
-                printf("The choice entered isn't valid.\n");
-                break;
+    else {
+        struct node *last;
+        last = temp;
+        if (l->last==NULL)
+        {
+            l->last = last;
+            l->first->next = last;
+            l->last->prev = l->first;
+            l->size = l->size + 1;
+        }  // first -next > last  && last - prev > first
+        else {
+            //
+            //
+            struct node *new_last;
+            new_last = temp;
+            new_last->prev = l->last;
+            l->last->next = new_last;
+            l->last = new_last;
+            l->size = l->size + 1;
         }
     }
+}
+void remove_by_position(struct list l,int index  ) {
+    if( l.size != 0) {
+        if ( index == 0 ) {
+              l.first->next->prev = NULL;
+              struct node *temp = l.first->next;
+              free(l.first);
+              l.first = temp;
+          } else if( index == l.size-1 ){
+              l.last->prev->next = NULL;
+              free(l.last);
+              l.last = l.last->prev;
+          }
+            else  {
+            struct node* temp = l.first;
+            int i = 0 ;
+            while ( i < index) {
+                i++;
+                temp = temp->next;
+            }
+            temp->prev->next = temp->next;
+            temp->next->prev = temp->prev;
+            free(temp);
+          }
+    } else printf("Database empty");
+}
+void display(struct list l){
+    if (l.size!=0) {struct node *p;
+        p = l.first;
+        while ( p != NULL ) {
+            printf("%s %s %s %s %s %s %s %.2f\n",p->val->name,p->val->type,p->val->cpu,p->val->ram, // do not like this
+                   p->val->storage,p->val->gpu,p->val->motherboard,p->val->price); // but too lazy to write 2 printfs
+            p = p->next;
+        }
+
+    }else printf("empty\n");
+
+}
+void add_pc(struct list* db) {
+    char *name = malloc(128*sizeof(char));
+    char *type = malloc(128*sizeof(char));
+    char *cpu = malloc(128*sizeof(char));
+    char *ram = malloc(128*sizeof(char));
+    char *storage = malloc(128*sizeof(char));
+    char *gpu = malloc(128*sizeof(char));
+    char *mobo = malloc(128*sizeof(char));
+    float p;
+    while ( 1 ) {
+        fflush(stdin);
+        printf("Name:\n");
+        gets(name);
+        printf("??\n");
+        gets(type);
+        printf("??\n");
+        gets(cpu);
+        printf("??\n");
+        gets(ram);
+        printf("??\n");
+        gets(storage);
+        printf("??\n");
+        gets(gpu);
+        printf("??\n");
+        gets(mobo);
+        printf("float\n");
+        scanf("%f",&p);
+        append(db,make_node(name,type,cpu,ram,storage,gpu,mobo,p));
+        printf("y/n\n");
+        int a = 0;
+        scanf("%d",&a);
+        if (a == 0 ) break;
+    }
+    free(name);free(type);free(cpu);free(ram);free(storage);free(gpu);free(mobo);
+}
+int main() {
+    struct list db = make_list();
+    int choice = 1;
+    while ( choice != 0 ) {
+      fflush(stdin);
+      printf("choice\n");
+      scanf("%d",&choice);
+      switch ( choice ) {
+        case 1:
+        add_pc(&db);
+      }
+   }
+
+    display(db);
+    remove_by_position(db,db.size-1);
+    display(db);
 }
